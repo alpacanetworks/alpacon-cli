d1e6d9348c3f8c237a9bb38b85173fb2707b4367
royroyee <kim131325@naver.com>
2023-12-13 16:46:38 +0900

    Refactor client request function

--- client/client.go
+++ client/client.go
@@ -95,12 +95,12 @@ func (ac *AlpaconClient) sendRequest(req *http.Request) ([]byte, error) {
 	}
 
 	// Alpacon API Server always return 201 on POST request
-	if req.Method == "POST" {
-		if resp.StatusCode != http.StatusCreated {
-			return nil, fmt.Errorf("expected status 201 for POST, got %s", resp.Status)
-		}
+	if req.Method == "POST" && resp.StatusCode != http.StatusCreated {
+		return nil, fmt.Errorf("POST request failed: expected status 201, got %d (%s)", resp.StatusCode, resp.Status)
+	} else if req.Method == "DELETE" && resp.StatusCode != http.StatusNoContent {
+		return nil, fmt.Errorf("DELETE request failed: expected status 204, got %d (%s). Verify permissions.", resp.StatusCode, resp.Status)
 	} else if resp.StatusCode < http.StatusOK || resp.StatusCode >= http.StatusMultipleChoices {
-		return nil, fmt.Errorf("unexpected status code: %s", resp.Status)
+		return nil, fmt.Errorf("unexpected status code: %d (%s)", resp.StatusCode, resp.Status)
 	}
 
 	return body, nil
