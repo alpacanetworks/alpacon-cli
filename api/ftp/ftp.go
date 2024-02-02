e82abe316e4ad0555323af6f9e50d62d347154ae
royroyee <kim131325@naver.com>
2024-02-02 22:59:07 +0900

    Add support for multiple file upload and download in cp command

--- api/ftp/ftp.go
+++ api/ftp/ftp.go
@@ -3,6 +3,7 @@ package ftp
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"github.com/alpacanetworks/alpacon-cli/api/server"
 	"github.com/alpacanetworks/alpacon-cli/client"
 	"github.com/alpacanetworks/alpacon-cli/utils"
@@ -19,109 +20,114 @@ const (
 	downloadAPIURL = "/api/websh/downloads/"
 )
 
-func UploadFile(ac *client.AlpaconClient, src string, dest string) error {
-	parts := strings.SplitN(dest, ":", 2)
-	serverName := parts[0]
-	remotePath := parts[1]
+func UploadFile(ac *client.AlpaconClient, src []string, dest string) error {
+	serverName, remotePath := splitPath(dest)
 
 	serverID, err := server.GetServerIDByName(ac, serverName)
 	if err != nil {
 		return err
 	}
 
-	content, err := utils.ReadFileFromPath(src)
-	if err != nil {
-		return err
-	}
+	for _, path := range src {
+		var requestBody bytes.Buffer
+		multiPartWriter := multipart.NewWriter(&requestBody)
+		defer multiPartWriter.Close()
 
-	var requestBody bytes.Buffer
-	multiPartWriter := multipart.NewWriter(&requestBody)
+		if err = multiPartWriter.WriteField("path", remotePath); err != nil {
+			return err
+		}
+		if err = multiPartWriter.WriteField("server", serverID); err != nil {
+			return err
+		}
 
-	err = multiPartWriter.WriteField("path", remotePath)
-	if err != nil {
-		return err
-	}
+		content, err := utils.ReadFileFromPath(path)
+		if err != nil {
+			return err
+		}
 
-	fileWriter, err := multiPartWriter.CreateFormFile("content", src)
-	if err != nil {
-		return err
-	}
-	_, err = fileWriter.Write(content)
-	if err != nil {
-		return err
-	}
+		fileWriter, err := multiPartWriter.CreateFormFile("content", filepath.Base(path))
+		if err != nil {
+			return err
+		}
+		if _, err = fileWriter.Write(content); err != nil {
+			return err
+		}
 
-	err = multiPartWriter.WriteField("server", serverID)
-	if err != nil {
-		return err
-	}
-	multiPartWriter.Close()
+		if err = multiPartWriter.Close(); err != nil {
+			return err
+		}
 
-	err = ac.SendMultipartRequest(uploadAPIURL, multiPartWriter, requestBody)
-	if err != nil {
-		return err
+		if err = ac.SendMultipartRequest(uploadAPIURL, multiPartWriter, requestBody); err != nil {
+			return err
+		}
 	}
 
 	return nil
 }
 
 func DownloadFile(ac *client.AlpaconClient, src string, dest string) error {
-	parts := strings.SplitN(src, ":", 2)
-	serverName := parts[0]
-	remotePath := parts[1]
+	serverName, remotePathStr := splitPath(src)
+
+	var remotePaths []string
+
+	trimmedPathStr := strings.Trim(remotePathStr, "\"")
+	remotePaths = strings.Fields(trimmedPathStr)
 
 	serverID, err := server.GetServerIDByName(ac, serverName)
 	if err != nil {
 		return err
 	}
 
-	downloadRequest := &DownloadRequest{
-		Path:   parts[1],
-		Server: serverID,
-	}
+	for _, path := range remotePaths {
+		downloadRequest := &DownloadRequest{
+			Path:   path,
+			Server: serverID,
+		}
 
-	postBody, err := ac.SendPostRequest(downloadAPIURL, downloadRequest)
-	if err != nil {
-		return err
-	}
-
-	var downloadResponse DownloadResponse
-	err = json.Unmarshal(postBody, &downloadResponse)
-	if err != nil {
-		return err
-	}
-
-	utils.CliWarning("Awaits file transfer completion from Alpacon server. Transfer may timeout after 100 seconds. If the specified file is not found, it will not download even after 100 seconds.")
-
-	maxAttempts := 100
-	var resp *http.Response
-
-	for count := 0; count < maxAttempts; count++ {
-		resp, err = ac.SendGetRequestForDownload(utils.RemovePrefixBeforeAPI(downloadResponse.DownloadURL))
+		postBody, err := ac.SendPostRequest(downloadAPIURL, downloadRequest)
 		if err != nil {
 			return err
 		}
 
-		if resp.StatusCode == http.StatusNotFound {
-			time.Sleep(1 * time.Second)
-			continue
+		var downloadResponse DownloadResponse
+		err = json.Unmarshal(postBody, &downloadResponse)
+		if err != nil {
+			return err
 		}
 
-		if resp.StatusCode == http.StatusOK {
-			break
+		utils.CliWarning(fmt.Sprintf("Awaits %s file transfer completion from Alpacon server. Transfer may timeout after 100 seconds. If the specified file is not found, it will not download even after 100 seconds.", path))
+
+		maxAttempts := 100
+		var resp *http.Response
+
+		for count := 0; count < maxAttempts; count++ {
+			resp, err = ac.SendGetRequestForDownload(utils.RemovePrefixBeforeAPI(downloadResponse.DownloadURL))
+			if err != nil {
+				return err
+			}
+
+			if resp.StatusCode == http.StatusOK {
+				break
+			} else {
+				time.Sleep(time.Second * 1)
+			}
+
+			if count == maxAttempts-1 {
+				return fmt.Errorf("%d attempts", maxAttempts)
+			}
 		}
-	}
 
-	defer resp.Body.Close()
+		defer resp.Body.Close()
 
-	respBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
+		respBody, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
 
-	err = utils.SaveFile(filepath.Join(dest, filepath.Base(remotePath)), respBody)
-	if err != nil {
-		return err
+		err = utils.SaveFile(filepath.Join(dest, filepath.Base(path)), respBody)
+		if err != nil {
+			return err
+		}
 	}
 
 	return nil
--- cmd/ftp/cp.go
+++ cmd/ftp/cp.go
@@ -9,32 +9,33 @@ import (
 )
 
 var CpCmd = &cobra.Command{
-	Use:   "cp [SOURCE] [DESTINATION]",
+	Use:   "cp [SOURCE...] [DESTINATION]",
 	Short: "Copy files between local and remote locations",
 	Long: `The cp command allows you to copy files between your local machine and a remote server.
 	Example usages:
-	- Upload: alpacon cp /path/to/local/file.txt servername:/remote/path/
-	- Download: alpacon cp servername:/remote/file.txt /local/path/`,
-	Example: `
-	Upload: alpacon cp /path/to/local/file.txt servername:/remote/path/
-	Download: alpacon cp servername:/remote/file.txt /local/path/
-	`,
-	Args: cobra.ExactArgs(2),
+	- Upload: alpacon cp /path/to/local/file1.txt /path/to/local/file2.txt servername:/remote/path/
+	- Download: alpacon cp servername:"/remote/path1 /remote/path2" /local/destination/path`,
 	Run: func(cmd *cobra.Command, args []string) {
-		src := args[0]
-		dest := args[1]
+		if len(args) < 2 {
+			utils.CliError("You must specify at least two arguments.")
+			return
+		}
+
+		dest := args[len(args)-1]
+		sources := args[:len(args)-1]
 
 		alpaconClient, err := client.NewAlpaconAPIClient()
 		if err != nil {
 			utils.CliError("Connection to Alpacon API failed: %s. Consider re-logging.", err)
+			return
 		}
 
-		if isRemotePath(src) && isLocalPath(dest) {
-			downloadFile(alpaconClient, src, dest)
-		} else if isLocalPath(src) && isRemotePath(dest) {
-			uploadFile(alpaconClient, src, dest)
+		if isLocalPaths(sources) && isRemotePath(dest) {
+			uploadFile(alpaconClient, sources, dest)
+		} else if isRemotePath(sources[0]) && isLocalPath(dest) {
+			downloadFile(alpaconClient, sources[0], dest)
 		} else {
-			utils.CliError("Invalid combination of source and destination paths. Please use the format 'cp [SOURCE] [DESTINATION]' for copying files.")
+			utils.CliError("Invalid combination of source and destination paths.")
 		}
 	},
 }
@@ -51,7 +52,16 @@ func isLocalPath(path string) bool {
 	return !isRemotePath(path)
 }
 
-func downloadFile(client *client.AlpaconClient, src, dest string) {
+func isLocalPaths(paths []string) bool {
+	for _, path := range paths {
+		if isRemotePath(path) {
+			return false
+		}
+	}
+	return true
+}
+
+func downloadFile(client *client.AlpaconClient, src string, dest string) {
 	err := ftp.DownloadFile(client, src, dest)
 	if err != nil {
 		utils.CliError("Failed to download the file from server: %s", err)
@@ -60,11 +70,10 @@ func downloadFile(client *client.AlpaconClient, src, dest string) {
 	utils.CliInfo("`%s` successfully downloaded from server `%s`", src, dest)
 }
 
-func uploadFile(client *client.AlpaconClient, src, dest string) {
+func uploadFile(client *client.AlpaconClient, src []string, dest string) {
 	err := ftp.UploadFile(client, src, dest)
 	if err != nil {
 		utils.CliError("Failed to upload the file to server %s", err)
-		return
 	}
 	utils.CliInfo("`%s` successfully uploaded to `%s` ", src, dest)
 }
