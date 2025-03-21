0fc8ef3cbbc0ed602231d2d5b7c3c09e93b6cd76
parkm2ngyu00 <parkm2ngyu00@naver.com>
2025-03-21 13:31:13 +0900

    Update types for UploadResponse

--- api/ftp/types.go
+++ api/ftp/types.go
@@ -25,15 +25,15 @@ type DownloadResponse struct {
 }
 
 type UploadResponse struct {
-	Id          string    `json:"id"`
-	Name        string    `json:"name"`
-	Path        string    `json:"path"`
-	Size        int       `json:"size"`
-	Server      string    `json:"server"`
-	User        string    `json:"user"`
-	Username    string    `json:"username"`
-	Groupname   string    `json:"groupname"`
-	ExpiresAt   time.Time `json:"expires_at"`
-	DownloadUrl string    `json:"download_url"`
-	Command     string    `json:"command"`
+	Id        string    `json:"id"`
+	Name      string    `json:"name"`
+	Path      string    `json:"path"`
+	Size      int       `json:"size"`
+	Server    string    `json:"server"`
+	User      string    `json:"user"`
+	Username  string    `json:"username"`
+	Groupname string    `json:"groupname"`
+	ExpiresAt time.Time `json:"expires_at"`
+	UploadUrl string    `json:"upload_url"`
+	Command   string    `json:"command"`
 }
